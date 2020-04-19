70c5f903ef5e55f7c9bbe500bf88268bc4f15db0
mthang1801 <mthang1801@gmail.com>
2020-04-19 21:57:43 +0700

    prepare for deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const authRouter = require("./routes/api/auth");
 const postsRouter = require("./routes/api/posts");
 const profileRouter = require("./routes/api/profile");
 const usersRouter = require("./routes/api/users");
+const path = require("path");
 const app = express();
 //connect MongoDB
 connectDB()
@@ -17,5 +18,13 @@ app.use("/api/users", usersRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/posts", postsRouter);
 
+//Serve static assets in production
+if(process.env.NODE_ENV === "production"){
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*" , (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
 const port = process.env.PORT || 5000;
 app.listen(port , console.log(`Server is running on port ${port}`));
